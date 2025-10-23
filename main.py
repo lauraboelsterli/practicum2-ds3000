642e1960967617884ae136abaada6eebb657953c
maxschaefer0318 <maxschaefer0318@gmail.com>
2025-10-23 13:07:05 -0400

    question 1 test

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("test test")
+"""
+Question #1
+
+The goal is to analyze the FAA Wildlife Strike Database to identify patterns and trends in wildlife
+strikes to civil aircraft. We must clean and reduce the dataset to only include relevant features for
+the analysis. We must examine factors such as aircraft type, wildlife involved, location, and time.
+After extracting factors like these we perform statistical tests, test hypotheses, and create visualizations
+to help reduce the occurance and impact of wildlife strikes on civil aircraft in the future.
+
+"""
+
+
+
 
